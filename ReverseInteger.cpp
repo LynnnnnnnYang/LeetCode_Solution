a7b53eb229dc70402a62a91cf4e34c2ecac1992d
LynnnnnnnYang <lynnnnnnn_yang@163.com>
2016-08-08 16:32:22 +0800

    Add files via upload

---
+++ ReverseInteger.cpp
@@ -0,0 +1,54 @@
+// @Source : https://leetcode.com/problems/reverse-integer/
+// @Author : LynnnnnnnYang
+// @Date   : 2016-08-08
+
+/**********************************************************************************
+* 7.Reverse Integer
+* Reverse digits of an integer.
+*
+* Example1: 
+* x = 123, return 321
+* Example2: 
+* x = -123, return -321
+**********************************************************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) 
+	{
+		int flag = 0;
+		long long res = 0;
+
+		//int: -2147483648 ~ 2147483647
+		if(x==-2147483648)	return 0;
+
+		if(x < 0) {x = -x;flag = 1;}
+
+		for(;x;x/=10)
+		{
+			res = res*10 + x % 10; 
+		}
+		if(res > 2147483647) return 0;
+
+		if(flag) res = -res;
+        return res;
+    }
+};
+
+int ReverseInteger_main()
+{
+    Solution sol;
+	
+	int n;
+	cin >> n;
+
+	cout<<n<<" reverse to: "<<sol.reverse(n)<<endl;
+	
+	system("pause");
+    return 0;
+}
---
+++ ZigZagConversion.cpp
@@ -0,0 +1,89 @@
+// @Source : https://leetcode.com/problems/zigzag-conversion/
+// @Author : LynnnnnnnYang
+// @Date   : 2016-08-08
+
+/**********************************************************************************
+* 6.ZigZag Conversion
+* The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+* (you may want to display this pattern in a fixed font for better legibility)
+* P   A   H   N
+* A P L S I I G
+* Y   I   R
+* And then read line by line: "PAHNAPLSIIGYIR"
+* 
+* Write the code that will take a string and make this conversion given a number of rows:
+* string convert(string text, int nRows);
+* 
+* Examples:
+* convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+**********************************************************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) 
+	{
+		string res = "";
+		int pos = 0,cur = 0;
+		int len = s.length();
+
+		if(numRows == 1) return s;
+
+		// i = 0
+		res.push_back(s[cur]);
+		cur += 2 * (numRows - 1);
+		while(cur < len)
+		{
+			res.push_back(s[cur]);
+			cur += 2 * (numRows - 1);
+		}
+
+		for(int i = 1;i < numRows-1;i++)
+		{
+			cur = i;
+			if(cur < len) res.push_back(s[cur]);
+
+			cur += 2 * (numRows - 1 - i);
+			while(cur < len)
+			{
+				res.push_back(s[cur]);
+
+				cur += 2 * i;
+				if(cur >= len) break;
+				res.push_back(s[cur]);
+
+				cur += 2 * (numRows - 1 - i);
+			}
+		}
+
+		// i = numRows - 1
+		cur = numRows - 1;
+		if(cur < len) res.push_back(s[cur]);
+		cur += 2 * (numRows - 1);
+		while(cur < len)
+		{
+			res.push_back(s[cur]);
+			cur += 2 * (numRows - 1);
+		}
+
+		return res;
+    }
+};
+
+int ZigZagConversion_main()
+{
+    Solution sol;
+	
+	string str;
+	int n;
+	cin >> str >> n;
+
+	cout<<str<<" convert to: "<<sol.convert(str,n)<<endl;
+	
+	system("pause");
+    return 0;
+}
